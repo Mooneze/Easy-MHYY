c876ffaae6f2701c07c8bcc4bd53b83685ddc536
GuangChen2333 <guangchenworks@outlook.com>
2024-05-26 12:27:03 +0800

    feat: Fully support the games and automatic

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+import questionary
+import asyncio
+from DrissionPage import WebPage
 
 
-# 按装订区域中的绿色按钮以运行脚本。
+async def listen(choice: int):
+    if choice == 0:
+        page.listen.start("https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/listNotifications")
+        page.get("https://ys.mihoyo.com/cloud/#/")
+    elif choice == 1:
+        page.listen.start("https://cg-hkrpg-api.mihoyo.com/hkrpg_cn/cg/gamer/api/listNotifications")
+        page.get("https://sr.mihoyo.com/cloud/#/")
+
+    for packet in page.listen.steps():
+        result = "X-Rpc-App_id" in packet.request.headers
+        print(f"> {packet.url} -> {result}")
+
+        if result:
+            return packet.request.headers
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    r = questionary.select(
+        "请选择你的游戏类型：",
+        choices=[
+            questionary.Choice("云·原神", 0),
+            questionary.Choice("云·星穹铁道", 1)
+        ]
+    ).ask()
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    page = WebPage()
+
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(listen(r))
+
+    print("请完成登录并等待...")
+
+    loop.run_until_complete(task)
+
+    headers = task.result()
+
+    if headers is None:
+        print("未完成登录，请重启程序后重试。")
+        exit(-1)
+
+    # 筛选关键 Headers
+    print("X-Rpc-Combo_token: " + headers["x-rpc-combo_token"])
+    print("X-RPC-Sys_version: " + headers["x-rpc-sys_version"])
+    print("X-Rpc-Device_id: " + headers["x-rpc-device_id"])
+    print("X-Rpc-Device_model: " + headers["x-rpc-device_model"])
+    print("X-Rpc-Device_name: " + headers["x-rpc-device_name"])
